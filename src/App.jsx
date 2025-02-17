9888833b5b6e91b0e0370269d6f7d2e446eb820b
diolemos <pedro.devsc@gamil.com>
2025-02-17 08:48:18 -0300

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Input from "./Input"
 function App() {
   const [image, setImage] = useState(null);
   const [overlay, setOverlay] = useState("");
+  const [preview, setPreview] = useState(sessionStorage.getItem("playmatPreview"));
 
   const handleSelectOverlay = (userChoice)=>{
       setOverlay(userChoice)
@@ -14,10 +15,32 @@ function App() {
     setImage(file);
   };
 
+  const handleGenerate = () => {
+    if (!image) return;
+    
+    const formData = new FormData();
+    formData.append("file", image);
+    formData.append("overlay", overlay);
+   
+    
+    fetch("http://127.0.0.1:8000/upload/", {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.blob())
+      .then((blob) => {
+        const url = URL.createObjectURL(blob);
+        setPreview(url);
+        sessionStorage.setItem("playmatPreview", url);
+      })
+      .catch((err) => console.error("Error uploading image:", err));
+  };
+
   
   return (
     <>
-     <Input onImageUpload={handleImageUpload}  onGenerate onSelectOverlay={handleSelectOverlay}/> 
+     <Input onImageUpload={handleImageUpload}  onGenerate={handleGenerate} onSelectOverlay={handleSelectOverlay}/> 
+    {/* display preview */}
     </>
   )
 }
