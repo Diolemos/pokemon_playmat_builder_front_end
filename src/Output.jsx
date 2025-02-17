8fda5d433c0777cf6dc64692af0e9c3bcd65f777
diolemos <pedro.devsc@gamil.com>
2025-02-17 09:43:16 -0300

    update styles

--- src/Output.jsx
+++ src/Output.jsx
@@ -3,7 +3,7 @@ export default function Output({preview,onDownload}){
 
     return (
         <div className="flex flex-col items-center text-center">
- {preview && <img src={preview} alt="Playmat Preview" className="max-w-full border" />}
+ {preview && <img src={preview} alt="Playmat Preview" className="w-full max-w-[500px] border" />}
       {preview && (
         <div className=" my-4 flex space-x-4">
           <button className="bg-blue-500 text-white px-4 py-2" onClick={onDownload}>
