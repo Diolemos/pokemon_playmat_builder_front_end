88d5bed7f8a10ab6429a2e73bac782a7327d0ae9
diolemos <pedro.devsc@gamil.com>
2025-02-17 08:37:03 -0300

    Update Input.jsx create handleImageUplaod

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Input from "./Input"
 
 function App() {
+  const [image, setImage] = useState(null);
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    setImage(file);
+  };
+
   
   return (
     <>
-     <Input/> 
+     <Input onImageUpload={handleImageUpload} overlay onGenerate onSelectOverlay/> 
     </>
   )
 }
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,16 +1,17 @@
-export default function Input(handleImageUpload, overlay, handleGenerate, handleSelectOverlay){
+export default function Input(onImageUpload, overlay, onGenerate, onSelectOverlay){
 
 
 
 
 
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center text-center">
           {/* Custom File Input */}
           <div className="flex flex-col my-4">
             <label className="mb-2 text-lg font-semibold">Upload Your Image</label>
             
             <label 
+             
               htmlFor="file-upload" 
               className="cursor-pointer bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition"
             >
@@ -20,7 +21,7 @@ export default function Input(handleImageUpload, overlay, handleGenerate, handle
               id="file-upload"
               type="file"
               accept="image/*"
-              onChange={handleImageUpload}
+              onChange={onImageUpload}
               className="hidden"
             />
           </div>
@@ -28,17 +29,17 @@ export default function Input(handleImageUpload, overlay, handleGenerate, handle
           {/* Overlay Selector */}
           <select 
             value={overlay} 
-            onChange={(e) => handleSelectOverlay(e.target.value)} 
-            className="border border-gray-300 rounded-md p-2 my-2"
+            onChange={(e) => onSelectOverlay(e.target.value)} 
+            className="border  border-gray-300 rounded-md cursor-pointer p-2 my-2"
           >
-            <option value="black">Black Overlay</option>
+            <option  value="black">Black Overlay</option>
             <option value="white">White Overlay</option>
           </select>
     
           {/* Generate Button */}
           <button 
             className="cursor-pointer bg-gray-500 text-white px-4 py-2 rounded-lg hover:bg-gray-700 transition"
-            onClick={handleGenerate}
+            onClick={onGenerate}
           >
             Generate
           </button>
